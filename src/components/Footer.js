bf2594ea92235b15c06da9a52a4910c603dc15e9
crycetruly <crycetruly@gmail.com>
2019-09-24 08:12:32 +0300

    feat(ui):Revamp UI

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,26 @@
 import React from 'react'
 
-export default props =>
-  <footer className="footer">
-    <span className="todo-count">
-      <strong>{props.incompleteTodos.length}</strong> todos left
+export default props =>{
+const {incompleteTodos,todos}=props;
+let completion=((todos.length-incompleteTodos.length)/todos.length)*100
+
+return(
+  <footer className="container">
+    <span className="mr-4">
+      <strong className='badge badge-lg badge-primary'>{incompleteTodos.length}</strong> Todos left
     </span>
-    <ul className="filters">
-      <li>All</li>{''} {props.todos.length}
-    </ul>
+    <span>All Todos</span>{''} <span className='badge badge-lg badge-info'>{todos.length}</span>
+
+
+<div className='mt-4'>
+<h5>Completed {todos.length-incompleteTodos.length} of {todos.length} tasks</h5>
+<div className="progress">
+    
+    <div className="progress-bar" role="progressbar" style={{width: `${completion}%`}} aria-valuenow={25} aria-valuemin={0} aria-valuemax={100} />
+  </div>
+</div>
+   
   </footer>
+
+);
+}
